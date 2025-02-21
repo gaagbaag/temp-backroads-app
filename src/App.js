cadd07bcb872a07af7122093db2486812fdd8c62
gaagbaag <gabrielaguayo@gmail.com>
2025-02-21 10:24:34 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Tours from "./components/Tours";
+import Services from "./components/Services";
+import About from "./components/About";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <About />
+      <Services />
+      <Tours />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/PageLink.js
@@ -0,0 +1,13 @@
+import { pageLinks } from "../data";
+
+const PageLink = ({ link, itemClass }) => {
+  return (
+    <li key={link.id}>
+      <a href={link.href} className={itemClass}>
+        {link.text}
+      </a>
+    </li>
+  );
+};
+
+export default PageLink;
